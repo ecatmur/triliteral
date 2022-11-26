4900b5f91ac24fa847d05d53c6af93ec103b54d1
Ed Catmur <ed@catmur.uk>
2022-11-26 18:05:40 +0000

    more scripts

--- triliteral.py
+++ triliteral.py
@@ -11,18 +11,32 @@ args = argparse.Namespace()
 returns = []
 script = None
 LATIN = dict(
-    values=dict(A=1, B=2, G=3, D=4, H=5, E=5, U=6, V=6, W=6, Z=7, C=8, Ch=8, T=9,
+    values=dict(
+        A=1, B=2, G=3, D=4, H=5, E=5, U=6, V=6, W=6, Z=7, C=8, Ch=8, T=9,
         I=10, J=10, Y=10, K=20, L=30, M=40, N=50, S=60, X=60, O=70, F=80, P=80, Ph=80, Ts=90, Tz=90,
-        Q=100, R=200, Sh=300, Th=400, K_=500, M_=600, N_=700, P_=800, Ph_=800, Ts_=900, Tz_=900),
-    vowels={'': '0', 'A': '1', 'E': '2', 'I': '2', 'O': '3', 'U': '3'},
+        Q=100, R=200, Sh=300, Th=400, K_=500, M_=600, N_=700, P_=800, Ph_=800, Ts_=900, Tz_=900,
+    ),
+    vowels={'': 0, 'A': 1, 'E': 1, 'U': 2, 'I': 3},
     ext='.trl',
-    )
+)
 ARABIC=dict(
-    ext='.\u0637\u0631\u0644',
-    )
+    values=dict(
+        ا=1, ب=2, ج=3, د=4, ه=5, و=6, ز=7, ح=8, ط=9,
+        ي=10, ك=20, ل=30, م=40, ن=50, س=60, ع=70, ف=80, ص=90,
+        ق=100, ر=200, ش=300, ت=400, ث=500, خ=600, ذ=700, ض=800, ظ=900, غ=1000,
+    ),
+    vowels={'': 0, 'ا': 1, 'و': 2, 'ي': 3},
+    ext='.طرل',
+)
 HEBREW=dict(
-    ext='.\u05D8\u05E8\u05DC',
-    )
+    values=dict(
+        א=1, ב=2, ג=3, ד=4, ה=5, ו=6, ז=7, ח=8, ט=9,
+        י=10, כ=20, ל=30, מ=40, נ=50, ס=60, ע=70, פ=80, צ=90,
+        ק=100, ר=200, ש=300, ת=400, ך=500, ם=600, ן=700, ף=800, ץ=900,
+    ),
+    vowels={'': 0, 'א': 1, 'ה': 1, 'ו': 2, 'י': 3},
+    ext='.טרל',
+)
 
 
 @dataclasses.dataclass
@@ -46,39 +60,55 @@ def unpack(word):
     vowels_re = r'([^' + ''.join(vowels.keys()) + r'])'
     parts = re.split(vowels_re, word.upper())[:6]
     root = "".join(parts[1::2])
-    stem = int("".join(vowels.get(c[:1], c[:1]) for c in parts[4::-2]), 4)
+    stem = int("".join(str(vowels.get(c[:1], c[:1])) for c in parts[4::-2]), 4)
     return root, stem
 
 
 def gem(word):
     acc = 0
-    word = word.upper()
+    word = word.upper() + '_'
     values = script['values']
-    while word:
-        c, word = word[0], word[1:]
-        if len(word) == 0 and (cc := c + '_') in values:
-            acc += values[cc]
-        elif len(word) == 1 and (cc := c + word[0].lower() + '_') in values:
-            word = word[1:]
-            acc += values[cc]
-        elif len(word) >= 1 and (cc := c + word[0].lower()) in values:
-            word = word[1:]
-            acc += values[cc]
-        else:
-            acc += values[c]
+    while word and word != '_':
+        for i in 3, 2, 1:
+            c = word[:i].title()
+            if c in values:
+                break
+        word = word[i:]
+        acc += values[c]
     return acc
 
 
-def degem(n):
+def degem(n, scr=None):
+    if scr is None:
+        scr = script
+    sv, sw = script['values'], script['vowels']
     vowels, consonants = [], []
     while n > 0:
-        for c, v in reversed(script['values'].items()):
-            if c[-1] == '_' and consonants:
-                pass
-            elif v <= n:
+        for c, v in reversed(sv.items()):
+            if (c[-1] != '_' or not consonants) and (c + '_' not in sv or consonants) and v <= n:
                 n -= v
-                (vowels if c in script['vowels'] else consonants)[:0] = (c[:-1] if c[-1] == '_' else c)
+                (vowels if c in sw else consonants)[:0] = (c[:-1] if c[-1] == '_' else c)
     return ''.join(itertools.chain.from_iterable(itertools.zip_longest(vowels, consonants, fillvalue='')))
+
+
+def recode(word, to):
+    out = ''
+    sv, sw = script['values'], script['vowels']
+    tv, tw = to['values'], to['vowels']
+    word = word.upper() + '_'
+    while word and word != '_':
+        for i in 3, 2, 1:
+            c = word[:i].title()
+            if c in sv:
+                break
+        word = word[i:]
+        x = sv[c]
+        y = sw.get(c, 0)
+        try:
+            out += next(k for k, v in tv.items() if v == x and tw.get(k, 0) == y)
+        except StopIteration:
+            raise Exception(f"can't recode '{c}' to {args.recode}")
+    return out
 
 
 def quot(stack):
@@ -151,7 +181,9 @@ def add(stack):
 def sub(stack):
     w = stack.pop() if stack else ''
     x = stack.pop() if stack else ''
-    stack.append(degem(gem(w) - gem(x)))
+    y = degem(gem(w) - gem(x))
+    assert gem(y) == max(0, gem(w) - gem(x)), (w, x, y)
+    stack.append(y)
 
 
 def mult(stack):
@@ -247,8 +279,8 @@ def wword(stack):
 
 
 def rchar(stack):
-    n = ord(input())
-    stack.append(degem(n))
+    n = sys.stdin.read(1)
+    stack.append(degem(ord(n) if n else 0))
 
 
 def wchar(stack):
@@ -277,6 +309,8 @@ def run(path):
     if args.recode:
         to = {'arabic': ARABIC, 'hebrew': HEBREW, 'latin': LATIN}[args.recode]
         out = open(base + to['ext'], 'w')
+        for word in program:
+            print(recode(word, to), file=out)
         return
     stacks = collections.defaultdict(list)
     returns.append(Return(program, program, 0))
